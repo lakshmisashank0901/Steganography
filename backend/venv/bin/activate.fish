8916687d26cb1da91a6fba8426c4137d7d8f1810
Lakshmi Sashank <lakshmisashank@Lakshmis-MacBook-Pro.local>
2026-04-10 23:32:57 +0530

    Commit

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/lakshmisashank/Desktop/Steganography Local/backend/venv"
+set -gx VIRTUAL_ENV "/Users/lakshmisashank/Desktop/Steganography/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH

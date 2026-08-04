0b5f092a2dce609cf159f3a570dc7c3a1e38bc6e
Lakshmi Sashank <lakshmisashank@Lakshmis-MacBook-Pro.local>
2026-08-04 19:11:31 +0530

    Deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://127.0.0.1:8000/api/:path*",
+        destination: process.env.BACKEND_URL ? `${process.env.BACKEND_URL}/api/:path*` : "http://127.0.0.1:8000/api/:path*",
       },
     ];
   },
